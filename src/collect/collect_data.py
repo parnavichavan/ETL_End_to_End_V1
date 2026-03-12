f1a514de57d8a693eba966f1b9ba0eb381d4b084
parnavichavan <parnavi7890@gmail.com>
2026-03-12 17:25:50 +0530

    Refactor dataset download to use JSON credentials
    Updated the download_dataset function to load Kaggle API credentials from a JSON file instead of hardcoding them.

--- src/collect/collect_data.py
+++ src/collect/collect_data.py
@@ -1,34 +1,25 @@
-import os
-from kaggle.api.kaggle_api_extended import KaggleApi
-
-import os
+import os, json
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_dataset(dataset_name, save_path):
-    """
-    Download dataset from Kaggle using API.
-    dataset_name: str -> format: "owner/dataset-name"
-    save_path: str -> folder path to save dataset
-    """
-    # Create an instance of the Kaggle API
+    # Load kaggle.json from config folder
+    with open("config/kaggle.json") as f:
+        creds = json.load(f)
+
+    os.environ["KAGGLE_USERNAME"] = creds["username"]
+    os.environ["KAGGLE_KEY"] = creds["key"]
+
     api = KaggleApi()
-    
-    # Authenticate using the kaggle.json file
     api.authenticate()
 
-    # Create the target directory if it doesn't exist
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     print(f"Downloading {dataset_name} to {save_path} ...")
-    
-    # Download and unzip the files
     api.dataset_download_files(dataset_name, path=save_path, unzip=True)
-    
     print("✅ Download complete!")
 
 if __name__ == "__main__":
     dataset = "ranitsarkar01/porter-delivery-time-estimation-dataset"
     save_dir = "data/raw"
-
     download_dataset(dataset, save_dir)
